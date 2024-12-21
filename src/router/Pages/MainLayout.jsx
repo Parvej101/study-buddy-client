2475bb9008a7fd00b95d64e95d797b2105f24226
Parvej101 <you@example.com>
2024-12-22 00:10:06 +0600

    Project setup - react router, tailwind, daisyUi

---
+++ src/router/Pages/MainLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../../Components/Navbar';
+
+const MainLayout = () => {
+    return (
+        <div className='container mx-auto'>
+            <Navbar></Navbar>
+           <Outlet></Outlet>
+           
+        </div>
+    );
+};
+
+export default MainLayout;
